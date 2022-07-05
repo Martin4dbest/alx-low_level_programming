27e76109d3a0351238dcf2d73e6da2536cb0fea5
Martin4dbest <martin4dtruth@gmail.com>
2022-07-05 06:07:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,14 @@
  */
 int main(void)
 {
-_putchar('b');
-_putchar('o');
-_putchar('o');
-_putchar('k');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
