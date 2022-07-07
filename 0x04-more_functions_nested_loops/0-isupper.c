7cd075f41fa79cb60ebf1689fb6998f195928a91
Martin4dbest <martin4dtruth@gmail.com>
2022-07-07 07:26:10 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,14 @@
 
 /**
  * _isupper -  checks for uppercase character.
- * @c: c -  Variable
- * Return: Always 0.
+ * @c: c - input.
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
 
-	if (c >= 65 && c <= 90)
-	{
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
