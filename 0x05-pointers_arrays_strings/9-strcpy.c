eb5a433953aca6aa93b73e9d29b5ef8b1daa915c
Martin4dbest <martin4dtruth@gmail.com>
2022-07-12 18:37:16 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,16 +11,18 @@
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
  *
- * Return: A pointer to the destination string @dest.
+ * Return: A pointer to the destination string
  */
 char *_strcpy(char *dest, char *src)
 {
-int index = 0;
-while (src[index])
-{
+	int i;
 
-dest[index] = src[index];
-index++;
-}
-return (dest);
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
 }
